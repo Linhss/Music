b03671b14dd1c09320559331b81c81e25fb3e5c3
Linh <nguyentuanlinh432001@gmail.com>
2022-08-11 19:34:25 +0700

    uploadfile

--- Accset/main.js
+++ Accset/main.js
@@ -1,88 +1,41 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-const player = $('.player')
-const heading = $('header h2')
-const cdThumb = $('.cd-thumb')
-const audio = $('#audio')
-const cd = $('.cd');
-const playBtn = $('.btn-toggle-play')
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const playlist = $('.playlist')
 
-const app  = {
-    currentIndex: 0,    // current index officially definedBy
+const app = {
     songs: [
         {
-            name: 'Bánh Mì Không',
-            singer: 'Đạt G - Uyên DU',
-            path:'./music/datg-x-duuyen-official-mv.mp3',
-            image: './img/banh-mi-khong.jpg'
-        },
-        {
-            name: 'Lối Nhỏ',
-            singer: 'Đen Vâu',
-            path:'./music/loi-nho-ft-phuong-anh-dao-m-v.mp3',
-            image: './img/Loi-nho.jpg'
+            name: 'Bánh Mì Không ',
+            singer: ' Đạt G - Uyên DU ',
+            path: './music/datg-x-duuyen-official-mv.mp3',
+            image:'./Accset/img/Lời-bài-hát-bánh-mì-không.png'
         },
     ],
-    render: function(){
-        const htmls = this.songs.map(song => {
-            return `
-            <div class="song">
-            <div class="thumb" style="background-image: url('${song.image}')">
+
+    render: function () {
+        const html = this.songs.map(song => {
+            return`
+                <div class="song">
+                    <div class="thumb" style="background-image: url('${song.image})">
+                </div>
+                <div class="body">
+                    <h3 class="title">${song.name}</h3>
+                    <p class="author">${song.singer}</p>
+                </div>
+                <div class="option">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
             </div>
-            <div class="body">
-              <h3 class="title">${song.name}</h3>
-              <p class="author">${song.singer}</p>
-            </div>
-            <div class="option">
-              <i class="fas fa-ellipsis-h"></i>
-            </div>
-          </div>           
             `
         })
 
-        $('.playlist').innerHTML = htmls.join(''); 
-    }, 
-    defineproperty: function(){
-        Object.defineProperty(this, 'currentSong',{
-            get: function() {
-                return this.songs[this.currentIndex];
-            }
-        })
-    },
-
-    headleEvenl: function(){
-        const cdWidth = cd.offsetWidth
-        //sử lí phóng to thu nhỏ cd 
-        document.onscroll   = function(events) {
-            const scrollTop = window.scrollY || document.documentElement.scrollTop
-            const newCdWidth = cdWidth - scrollTop
-            cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0
-            cd.style.opacity = newCdWidth / cdWidth
-        }
-         //sử lí khi click play 
-        playBtn.onclick = function(){
-            audio.play()
-            player.classList.add('playing')
-        }
-    },
-    loadCurrentSong: function(){
-        heading.textContent = this.currentSong.name
-        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`
-        audio.src = this.currentSong.path
-           
+    //   console.log( playlist.innerHTML = html.join(''))
     },
 
     stast: function(){
-        // định nghĩa các thuộc tình cho objects
-        this.defineproperty()
-        // Lắng nghe / xử lí các sự kiện trong (DOM events)
-        this.headleEvenl();
-        // tải thông tin bài hát đầu tiên vào UI 
-        this.loadCurrentSong()
-        //render playlist
         this.render();
-    },
+    }
+    
 }
 
-app.stast()   
-
+app.stast()
