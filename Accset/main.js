5fba09a835f8ffaf22832c83667cd4ac72357d3d
Linh <nguyentuanlinh432001@gmail.com>
2022-08-14 20:56:32 +0700

    fix loi file img vs music

--- Accset/main.js
+++ Accset/main.js
@@ -19,38 +19,38 @@ const app = {
         {
             name: 'Bánh Mì Không ',
             singer: ' Đạt G - Uyên DU ',
-            path: '../Accset/music/banh-mi-khong.mp3',
-            image:'../Accset/img/bmk.png'
+            path: './Accset/music/banh-mi-khong.mp3',
+            image:'./Accset/img/bmk.png'
         },
         {
             name: 'Lối Nhỏ ',
             singer: ' Đen vâu',
-            path: '../Accset/music/loi-nho-ft-phuong-anh-dao-m-v.mp3',
-            image:'../Accset/img/ln.png'
+            path: './Accset/music/loi-nho-ft-phuong-anh-dao-m-v.mp3',
+            image:'./Accset/img/ln.png'
         },
         {
             name: 'Ánh Nắng Của Anh ',
             singer: 'Đức Phúc',
-            path: '../Accset/music/anh-nang-cua-anh.mp3',
-            image:'../Accset/img/anca.png'
+            path: './Accset/music/anh-nang-cua-anh.mp3',
+            image:'./Accset/img/anca.png'
         },
         {
             name: 'Duyên Mình Lỡ',
             singer: 'Hương Tràm',
-            path: '../Accset/music/duyen-minh-lo.mp3',
-            image:'../Accset/img/duyen_minh_lo.png'
+            path: './Accset/music/duyen-minh-lo.mp3',
+            image:'./Accset/img/duyen_minh_lo.png'
         },
         {
             name: 'Làm Người Yêu Anh Nhé BaBy',
             singer: 'Ba Chú Bộ Đội',
-            path: '../Accset/music/lam-nguoi-yeu-anh-nhe.mp3',
-            image:'../Accset/img/lnyanbb.png'
+            path: './Accset/music/lam-nguoi-yeu-anh-nhe.mp3',
+            image:'./Accset/img/lnyanbb.png'
         },
         {
             name: 'Mượn Rượi Tỏ Tình Cover',
             singer: 'Thanh Nhi',
-            path: '../Accset/music/muon-ruou-to-tinh.mp3',
-            image:'../Accset/img/mrtt.png'
+            path: './Accset/music/muon-ruou-to-tinh.mp3',
+            image:'./Accset/img/mrtt.png'
         },
     ],
 
